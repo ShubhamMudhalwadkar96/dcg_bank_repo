4acc0acc5a6eed50964036d7e1d1ddba2124292f
Shubham Mudhalwadkar <shubham.mudhalwadkar@yash.com>
2023-06-22 12:47:35 +0530

    Bug 100 Fix

--- Demo.java
+++ Demo.java
@@ -1,7 +1,15 @@
 public class Demo{
+<<<<<<< Updated upstream
 	
 	// Bug Fix - 101
 	public void m1(){
 		//logic
 	}
 }
+=======
+	// Bug Fix - 100
+	public void m1(){
+		//logic
+	}
+}
+>>>>>>> Stashed changes
